1102303e9e25be786dc86cd205890aad48cf1281
Victoria Ribeiro <victoriahenhen@gmail.com>
2020-09-26 20:02:23 -0300

    Incremento do projeto 03/atividade

--- 03-projeto-mulheres-ti/server.js
+++ 03-projeto-mulheres-ti/server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const app = express();
-const porta = 3000;
+const porta = 5000;
+const mulherestecnologia = require('./mulherestecnologia.json');
 
-const biografias = require('./rotas/biografias')
+// GET para visualizar todas as mulheres da lista
+app.get('/mulherestecnologia', (req, res) => {
+    res.json(mulherestecnologia);
+})
 
-app.use('/api/',biografias)
+// GET para visualizar a partir do ID
+app.get('/mulherestecnologia/:id', (req, res) => {
+    const {id} = req.params;
+    const idSelecionado = mulherestecnologia.find(idSelecionado => idSelecionado.id == id);
 
-app.use(express.json())
+    res.json(idSelecionado);
+})
 
-app.listen(porta, () => {
-  console.log(`Servidor rodando na porta: ${porta}`)
+app.use(express.json());
+
+app.listen(porta, function() {
+    console.log('Servidor rodando')
 })
